97a08d3fa264e117279daff9b1574625703e0241
Amandeep Singh <arshgrover32@gmail.com>
2022-02-23 17:18:05 +0530

    Conditional check on timeout option

--- src/Platform/AcquiaCloudPlatform.php
+++ src/Platform/AcquiaCloudPlatform.php
@@ -196,7 +196,7 @@ class AcquiaCloudPlatform extends PlatformBase implements PlatformSitesInterface
 
     $group_name = $input->getOption('group');
     $input_uri = $input->getOption('uri');
-    $process_timeout = (int) $input->getOption('timeout');
+    $process_timeout = $input->hasOption('timeout') && ($timeout = (int) $input->getOption('timeout')) > 3000 ? $timeout : 3000;
 
     if (!$input_uri && $group_name) {
       $alias = $this->getAlias();
